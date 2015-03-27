bfb49dd81b5dc570b5b26f4ab6f3eb08c6ce4f47
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-03-27 15:06:37 +0200

    Exclude unsupported tests on SMB3

--- devstack_vm/bin/run_tests.sh
+++ devstack_vm/bin/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+job_type=$1
+
 cd /opt/stack/tempest
 
 testr init
@@ -9,7 +11,11 @@ EXCLUDED_TESTS="$TEMPEST_DIR/excluded_tests.txt"
 RUN_TESTS_LIST="$TEMPEST_DIR/test_list.txt"
 mkdir -p "$TEMPEST_DIR"
 
-testr list-tests | grep volume > "$RUN_TESTS_LIST" || echo "failed to generate list of tests"
+if [ $job_type = "iscsi" ]; then
+    testr list-tests | grep volume > "$RUN_TESTS_LIST" || echo "failed to generate list of tests"
+else
+    testr list-tests | grep volume | grep -v test_snapshot_create_with_volume_in_use > "$RUN_TESTS_LIST" || echo "failed to generate list of tests"
+fi
 
 testr run --parallel --subunit  --load-list=$RUN_TESTS_LIST |  subunit-2to1  > /home/ubuntu/tempest/subunit-output.log 2>&1
 cat /home/ubuntu/tempest/subunit-output.log | /opt/stack/tempest/tools/colorizer.py > /home/ubuntu/tempest/tempest-output.log 2>&1
