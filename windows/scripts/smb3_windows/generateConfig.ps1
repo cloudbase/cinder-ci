ccbd3e31257fd9e04b2cc183dd8d29d1cfec5a5c
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-03-30 23:24:19 +0300

    update qemu-img

--- windows/scripts/smb3_windows/generateConfig.ps1
+++ windows/scripts/smb3_windows/generateConfig.ps1
@@ -7,6 +7,17 @@ Param(
     [Parameter(Mandatory=$true)][string]$lockPath
 )
 
+function unzip($src, $dest) {
+
+	$shell = new-object -com shell.application
+	$zip = $shell.NameSpace($src)
+	foreach($item in $zip.items())
+	{
+		$shell.Namespace($dest).copyhere($item)
+	}
+
+}
+
 $volumeDriver = 'cinder.volume.drivers.windows.smbfs.WindowsSmbfsDriver'
 $smbSharesConfigPath = "$configDir\smbfs_shares_config.txt"
 $configFile = "$configDir\cinder.conf"
@@ -20,3 +31,8 @@ $config = expand_template $template
 Write-Host "Config file:"
 Write-Host $config
 sc $configFile $config
+
+# FIX FOR qmeu-img - fetch locally compiled one
+Invoke-WebRequest -Uri http://dl.openstack.tld/qemu-img-cbsl-build.zip -OutFile c:\qemu-img\qemu-img-cbsl-build.zip
+mkdir c:\qemu2
+unzip c:\qemu-img\qemu-img-cbsl-build.zip c:\qemu2
