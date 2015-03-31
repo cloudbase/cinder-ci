59517c88a949df5556794627d22117e41779c200
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-03-31 18:09:12 +0300

    ensure uniformity

--- windows/scripts/iscsi/generateConfig.ps1
+++ windows/scripts/iscsi/generateConfig.ps1
@@ -4,7 +4,9 @@ Param(
     [Parameter(Mandatory=$true)][string]$devstackIP,
     [Parameter(Mandatory=$true)][string]$rabbitUser,
     [Parameter(Mandatory=$true)][string]$logDir,
-    [Parameter(Mandatory=$true)][string]$lockPath
+    [Parameter(Mandatory=$true)][string]$lockPath,
+    [Parameter(Mandatory=$true)][string]$username,
+    [Parameter(Mandatory=$true)][string]$password
 )
 
 $volumeDriver = 'cinder.volume.drivers.windows.windows.WindowsDriver'
