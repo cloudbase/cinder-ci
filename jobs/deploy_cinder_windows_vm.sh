a4ef59964286af0277edd6b584cf43863a7c87bc
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-03-31 18:16:21 +0300

    get required params to create-environment

--- jobs/deploy_cinder_windows_vm.sh
+++ jobs/deploy_cinder_windows_vm.sh
@@ -20,7 +20,7 @@ join_cinder(){
     echo "Ensure service is configured"
     run_ps_cmd_with_retry $PARAMS "C:\cinder-ci\windows\scripts\EnsureOpenStackServices.ps1 $WINDOWS_USER $WINDOWS_PASSWORD"
     echo "create cinder env on windows"
-    run_ps_cmd_with_retry $PARAMS "C:\cinder-ci\windows\scripts\create-environment.ps1 -devstackIP $FIXED_IP -branchName $ZUUL_BRANCH -buildFor $ZUUL_PROJECT -testCase $JOB_TYPE"
+    run_ps_cmd_with_retry $PARAMS "C:\cinder-ci\windows\scripts\create-environment.ps1 -devstackIP $FIXED_IP -branchName $ZUUL_BRANCH -buildFor $ZUUL_PROJECT -testCase $JOB_TYPE -winUser $WINDOWS_USER -winPasswd $WINDOWS_PASSWORD"
 }
 
 export CINDER_VM_NAME="cinder-windows-$UUID"
