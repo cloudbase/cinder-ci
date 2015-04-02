2e9a265f70f06a478472bc91697a68285c539ef6
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-04-02 15:11:40 +0300

    Update smb3_linux git fixes

--- devstack_vm/bin/run_devstack.sh
+++ devstack_vm/bin/run_devstack.sh
@@ -44,17 +44,17 @@ sudo easy_install -U pip
 cd /opt/stack/cinder
 git config --global user.email "microsoft_cinder_ci@microsoft.com"
 git config --global user.name "Microsoft Cinder CI"
-git remote add downstream https://github.com/alexpilotti/cinder-ci-fixes
-#git remote add downstream https://github.com/petrutlucian94/cinder
+#git remote add downstream https://github.com/alexpilotti/cinder-ci-fixes
+git remote add downstream https://github.com/petrutlucian94/cinder
 git fetch downstream
 git cherry-pick d99a73a6410a4a63b4818f387d7c561ca268db2f
 git cherry-pick d9e5d12258bac06e436605da7e3928808f9c98e0
 git cherry-pick c0ed2ab8cc6b1197e426cd6c58c3b582624d1cfd
 git cherry-pick 01fd56078bc4d73236dab02f6df0bd38b344834c
-git cherry-pick ae508692c7978e19743211290c1b2a8dfa63f75d
-git cherry-pick 184506b6db02f9f7e620ce340b74e391cc200f41
-git cherry-pick 73cb62a862ecf005192c5563d5782416dcf4aec9
-git cherry-pick 554810c3224c01edf7755f9b5809f59f5f73df23
+git cherry-pick 5ea88ec3fb90a520126743669697c957dccf7e96
+git cherry-pick ba51ca2f0dc46565cdd825c689607521ddea6c28
+git cherry-pick 401b44d6f9d45b74a688a6dc70dbefc9346a9fe4
+git cherry-pick 88313c535d4430fb7771965b7ab7f56a61d3aa6c
 
 cd /home/ubuntu/devstack
 
