6ea4db8265a1efa7c7f32e18c1ab432f562eb230
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-06-17 12:22:24 +0300

    fixed log messages and manual run cmd

--- windows/scripts/create-environment.ps1
+++ windows/scripts/create-environment.ps1
@@ -196,18 +196,25 @@ Start-Sleep -s 30
 if ($(get-service cinder-volume).Status -eq "Stopped")
 {
     Write-Host "We try to start:"
-    Write-Host "Start-Process -PassThru -RedirectStandardError $remoteLogs\process_error.txt -RedirectStandardOutput $remoteLogs\process_output.txt $pythonDir+\python.exe -c from ctypes import wintypes; from cinder.cmd import volume; volume.main()"
+    Write-Host Start-Process -PassThru -RedirectStandardError "$remoteLogs\process_error.txt" -RedirectStandardOutput "$remoteLogs\process_output.txt" -FilePath "$pythonDir\python.exe" -ArgumentList '-c "from ctypes import wintypes; from cinder.cmd import volume; volume.main()"'
     Try
     {
-    	$proc = Start-Process -PassThru -RedirectStandardError "$remoteLogs\process_error.txt" -RedirectStandardOutput "$remoteLogs\process_output.txt" $pythonDir+'\python.exe -c "from ctypes import wintypes; from cinder.cmd import volume; volume.main()"'
+    	$proc = Start-Process -PassThru -RedirectStandardError "$remoteLogs\process_error.txt" -RedirectStandardOutput "$remoteLogs\process_output.txt" -FilePath "$pythonDir\python.exe" -ArgumentList '-c "from ctypes import wintypes; from cinder.cmd import volume; volume.main()"'
     }
     Catch
     {
     	Throw "Could not start the process manually"
     }
     Start-Sleep -s 30
-    if (! $proc.HasExited) {Stop-Process -Id $proc.Id -Force}
-    Throw "Can not start the cinder-volume service"
+    if (! $proc.HasExited)
+    {
+    	Stop-Process -Id $proc.Id -Force
+    	Throw "Process started fine when run manually."
+    }
+    else
+    {
+    	Throw "Can not start the cinder-volume service. The manual run failed as well."
+    }
 }
 
 
