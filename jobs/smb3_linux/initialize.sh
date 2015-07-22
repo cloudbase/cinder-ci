bb4ca221ea3d129dbe47fa3ac3bf9f90cf9efda7
Victor Laza <vlaza@cloudbasesolutions.com>
2015-07-23 02:29:33 +0300

    Forcing dpkg -i cifs-utils if apt-get install fails

--- jobs/smb3_linux/initialize.sh
+++ jobs/smb3_linux/initialize.sh
@@ -13,8 +13,18 @@ run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP \
 run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP \
     $DEVSTACK_SSH_KEY "sudo echo //$DEVSTACK_FLOATING_IP/openstack/volumes -o guest > /etc/cinder/smbfs_shares_config" 1
 
+set +e
 run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP \
-    $DEVSTACK_SSH_KEY "sudo apt-get --assume-yes install cifs-utils"
+    $DEVSTACK_SSH_KEY "sudo apt-get --asume-yes -o Debug::pkgProblemResolver=true -o Debug::Acquire::http=true -f install cifs-utils"
+if [ $? -ne 0 ]; then
+run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP \
+    $DEVSTACK_SSH_KEY "sudo dpkg -i http://lug.mtu.edu/ubuntu/pool/main/c/cifs-utils/cifs-utils_6.0-1ubuntu2_amd64.deb"
+    exit_code=$?
+fi
+set -e
+if [ $exit_code -ne 0 ]; then
+    exit 1
+fi
 
 update_local_conf "/usr/local/src/cinder-ci/jobs/smb3_linux/local-conf-extra"
 
