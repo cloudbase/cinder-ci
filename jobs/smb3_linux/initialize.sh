8f0ad49e6ed9fbb2e0bbf63d552600284c7719e7
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-07-23 12:36:59 +0300

    installing cifs-utils is not smb3-linux specific

--- jobs/smb3_linux/initialize.sh
+++ jobs/smb3_linux/initialize.sh
@@ -12,20 +12,6 @@ run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP \
     $DEVSTACK_SSH_KEY "sudo mkdir -p /etc/cinder && sudo chown ubuntu /etc/cinder" 1
 run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP \
     $DEVSTACK_SSH_KEY "sudo echo //$DEVSTACK_FLOATING_IP/openstack/volumes -o guest > /etc/cinder/smbfs_shares_config" 1
-run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP \
-    $DEVSTACK_SSH_KEY "sudo wget http://dl.openstack.tld/cifs-utils_6.0-1ubuntu2_amd64.deb -O /tmp/cifs-utils_6.0-1ubuntu2_amd64.deb"
-set +e
-run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP \
-    $DEVSTACK_SSH_KEY "sudo apt-get --assume-yes -o Debug::pkgProblemResolver=true -o Debug::Acquire::http=true -f install cifs-utils"
-if [ $? -ne 0 ]; then
-run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP \
-    $DEVSTACK_SSH_KEY "sudo dpkg --install /tmp/cifs-utils_6.0-1ubuntu2_amd64.deb"
-    exit_code=$?
-fi
-set -e
-if [ $exit_code -ne 0 ]; then
-    exit 1
-fi
 
 update_local_conf "/usr/local/src/cinder-ci/jobs/smb3_linux/local-conf-extra"
 
