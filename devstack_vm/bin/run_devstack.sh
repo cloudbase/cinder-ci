243a05f0066a16679db2b625c0b1a06384826150
Victor Laza <vlaza@cloudbasesolutions.com>
2015-08-23 19:40:20 +0300

    Changed "nohup ./stack.sh" into "stack.sh | tee"
    * Doing this as from time to time wait() returns SEGFAULT and we don't see what crashed while stacking,
    * removed old backup code

--- devstack_vm/bin/run_devstack.sh
+++ devstack_vm/bin/run_devstack.sh
@@ -18,40 +18,10 @@ sudo easy_install -U pip
 sudo pip install -U six
 sudo pip install -U kombu
 
-#Running an extra apt-get update
-sudo apt-get update --assume-yes
-
 #Ensure subunit is available
 set +e
-exit_code=0
 sudo apt-get install subunit -y -o Debug::pkgProblemResolver=true -o Debug::Acquire::http=true -f
-# Backup install in case repository install fails
-if [ $? -ne 0 ]; then
-    echo "Using backup: manually installing packages"
-    wget --quiet --output-document=/tmp/libsubunit-perl_0.0.18-0ubuntu7_all.deb http://dl.openstack.tld/subunit_deps/libsubunit-perl_0.0.18-0ubuntu7_all.deb | sudo dpkg --install /tmp/libsubunit-perl_0.0.18-0ubuntu7_all.deb
-    wget --quiet --output-document=/tmp/python-testtools_0.9.35-0ubuntu1_all.deb http://dl.openstack.tld/subunit_deps/python-testtools_0.9.35-0ubuntu1_all.deb | sudo dpkg --install /tmp/python-testtools_0.9.35-0ubuntu1_all.deb
-    wget --quiet --output-document=/tmp/python-testscenarios_0.4-2ubuntu2_all.deb http://dl.openstack.tld/subunit_deps/python-testscenarios_0.4-2ubuntu2_all.deb | sudo dpkg --install /tmp/python-testscenarios_0.4-2ubuntu2_all.deb
-    wget --quiet --output-document=/tmp/python-extras_0.0.3-2ubuntu1_all.deb http://dl.openstack.tld/subunit_deps/python-extras_0.0.3-2ubuntu1_all.deb | sudo dpkg --install /tmp/python-extras_0.0.3-2ubuntu1_all.deb
-    wget --quiet --output-document=/tmp/python-subunit_0.0.18-0ubuntu7_all.deb http://dl.openstack.tld/subunit_deps/python-subunit_0.0.18-0ubuntu7_all.deb | sudo dpkg --install /tmp/python-subunit_0.0.18-0ubuntu7_all.deb
-    wget --quiet --output-document=/tmp/libatk1.0-0_2.10.0-2ubuntu2_amd64.deb http://dl.openstack.tld/subunit_deps/libatk1.0-0_2.10.0-2ubuntu2_amd64.deb | sudo dpkg --install /tmp/libatk1.0-0_2.10.0-2ubuntu2_amd64.deb
-    wget --quiet --output-document=/tmp/libcairo2_1.13.0~20140204-0ubuntu1_amd64.deb http://dl.openstack.tld/subunit_deps/libcairo2_1.13.0~20140204-0ubuntu1_amd64.deb | sudo dpkg --install /tmp/libcairo2_1.13.0~20140204-0ubuntu1_amd64.deb
-    wget --quiet --output-document=/tmp/libgdk-pixbuf2.0-0_2.30.7-0ubuntu1_amd64.deb http://dl.openstack.tld/subunit_deps/libgdk-pixbuf2.0-0_2.30.7-0ubuntu1_amd64.deb | sudo dpkg --install /tmp/libgdk-pixbuf2.0-0_2.30.7-0ubuntu1_amd64.deb
-    wget --quiet --output-document=/tmp/libgtk2.0-0_2.24.23-0ubuntu1_amd64.deb http://dl.openstack.tld/subunit_deps/libgtk2.0-0_2.24.23-0ubuntu1_amd64.deb | sudo dpkg --install /tmp/libgtk2.0-0_2.24.23-0ubuntu1_amd64.deb
-    wget --quiet --output-document=/tmp/libpango-1.0-0_1.36.3-1ubuntu1_amd64.deb http://dl.openstack.tld/subunit_deps/libpango-1.0-0_1.36.3-1ubuntu1_amd64.deb | sudo dpkg --install /tmp/libpango-1.0-0_1.36.3-1ubuntu1_amd64.deb
-    wget --quiet --output-document=/tmp/libpangocairo-1.0-0_1.36.3-1ubuntu1_amd64.deb http://dl.openstack.tld/subunit_deps/libpangocairo-1.0-0_1.36.3-1ubuntu1_amd64.deb | sudo dpkg --install /tmp/libpangocairo-1.0-0_1.36.3-1ubuntu1_amd64.deb
-    wget --quiet --output-document=/tmp/python-cairo_1.8.8-1ubuntu5_amd64.deb http://dl.openstack.tld/subunit_deps/python-cairo_1.8.8-1ubuntu5_amd64.deb | sudo dpkg --install /tmp/python-cairo_1.8.8-1ubuntu5_amd64.deb
-    wget --quiet --output-document=/tmp/python-gobject-2_2.28.6-12build1_amd64.deb http://dl.openstack.tld/subunit_deps/python-gobject-2_2.28.6-12build1_amd64.deb | sudo dpkg --install /tmp/python-gobject-2_2.28.6-12build1_amd64.deb
-    wget --quiet --output-document=/tmp/python-gtk2_2.24.0-3ubuntu3_amd64.deb http://dl.openstack.tld/subunit_deps/python-gtk2_2.24.0-3ubuntu3_amd64.deb | sudo dpkg --install /tmp/python-gtk2_2.24.0-3ubuntu3_amd64.deb
-    wget --quiet --output-document=/tmp/python-junitxml_0.6-1.1build1_all.deb http://dl.openstack.tld/subunit_deps/python-junitxml_0.6-1.1build1_all.deb | sudo dpkg --install /tmp/python-junitxml_0.6-1.1build1_all.deb
-    wget --quiet --output-document=/tmp/subunit_0.0.18-0ubuntu7_all.deb http://dl.openstack.tld/subunit_0.0.18-0ubuntu7_all.deb | sudo dpkg --install /tmp/subunit_0.0.18-0ubuntu7_all.deb
-#    wget --quiet --output-document=/tmp/ http://dl.openstack.tld/subunit_deps/ | sudo dpkg --install /tmp/
-    sudo apt-get -f install -y
-    exit_code=$?
-fi
 set -e
-if [ $exit_code -ne 0 ]; then
-    exit 1
-fi
 
 DEVSTACK_LOGS="/opt/stack/logs/screen"
 LOCALRC="/home/ubuntu/devstack/localrc"
@@ -100,7 +70,6 @@ function cherry_pick(){
 
 if [ $job_type != "iscsi" ]; then
     set +e
-    #git remote add downstream https://github.com/alexpilotti/cinder-ci-fixes
     git remote add downstream https://github.com/petrutlucian94/cinder
     git fetch downstream
     git checkout -b testBranch
@@ -119,13 +88,4 @@ cd /home/ubuntu/devstack
 
 ./unstack.sh
 
-nohup ./stack.sh > /opt/stack/logs/stack.sh.txt 2>&1 &
-pid=$!
-wait "$pid"
-stack_status=$?
-cat /opt/stack/logs/stack.sh.txt
-if [ $stack_status -ne 0 ]
-then
-    echo "stack_status is: $stack_status"
-    exit 1
-fi
+./stack.sh 2>&1 | tee /opt/stack/logs/stack.sh.txt
