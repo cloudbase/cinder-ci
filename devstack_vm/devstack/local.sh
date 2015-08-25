e8459876e1eea13b0fa0c7c9c754d8aa2bd4a81e
Victor Laza <vlaza@cloudbasesolutions.com>
2015-08-25 20:38:45 +0300

    Disable STP on bridge

--- devstack_vm/devstack/local.sh
+++ devstack_vm/devstack/local.sh
@@ -34,3 +34,6 @@ iniset $TEMPEST_CONFIG boto build_timeout 300
 
 iniset $TEMPEST_CONFIG compute ssh_timeout 180
 iniset $TEMPEST_CONFIG compute allow_tenant_isolation True
+
+# Disable STP on bridge
+ovs-vsctl set bridge br-eth1 stp_enable=true
