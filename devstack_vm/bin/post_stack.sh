a26e5e971fdc191cf48dad798e7abd9b7e8411f2
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-08-26 15:53:26 +0300

    enable NAT

--- devstack_vm/bin/post_stack.sh
+++ devstack_vm/bin/post_stack.sh
@@ -6,7 +6,7 @@ nova quota-class-update --instances 50 --cores 100 --ram $((51200*4)) --floating
 cinder quota-class-update --snapshots 50 --volumes 50 --gigabytes 2000 default
 
 #NAT
-#sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-#sudo /sbin/iptables -A FORWARD -i eth0 -o br-eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-#sudo /sbin/iptables -A FORWARD -i br-eth1 -o eth0 -j ACCEPT
+sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo /sbin/iptables -A FORWARD -i eth0 -o br-eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo /sbin/iptables -A FORWARD -i br-eth1 -o eth0 -j ACCEPT
 
