8d60936c6f2b3e179b41b58e34e890921675bfc2
petrutlucian94 <petrutlucian94@gmail.com>
2015-09-01 13:23:32 +0300

    Updated Windows cherry-picks

--- windows/scripts/create-environment.ps1
+++ windows/scripts/create-environment.ps1
@@ -170,7 +170,8 @@ if ($testCase -ne "iscsi"){
 
     git checkout -b "testBranch"
 
-    cherry_pick 04fbfd1aa7c4fead321bf91fd60bd8ee0c1c482f
+    git fetch https://plucian@review.openstack.org/openstack/cinder refs/changes/13/158713/18
+    cherry_pick FETCH_HEAD
     cherry_pick 82f169a0aec3fe5ba3f4fa87f36fe365ecf8f108
     cherry_pick 4fef430adbd6c1e40a885040b347e4c9c394c161
 }
