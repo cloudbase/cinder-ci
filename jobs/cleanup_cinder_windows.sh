7ec3058ea561c5256482155534543da63b660c49
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2015-09-08 17:29:18 +0300

    Skip cleaning up debug jobs

--- jobs/cleanup_cinder_windows.sh
+++ jobs/cleanup_cinder_windows.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 source $KEYSTONERC
 
-set +e
+if [[ -z $DEBUG_JOB ]] || [[ $DEBUG_JOB != 'yes' ]] ;then
+    set +e
 
-echo "Releasing cinder floating ip"
-nova remove-floating-ip "$CINDER_VM_NAME" "$CINDER_FLOATING_IP"
-echo "Removing cinder VM"
-nova delete "$CINDER_VM_NAME"
-echo "Deleting cinder floating ip"
-nova floating-ip-delete "$CINDER_FLOATING_IP"
+    echo "Releasing cinder floating ip"
+    nova remove-floating-ip "$CINDER_VM_NAME" "$CINDER_FLOATING_IP"
+    echo "Removing cinder VM"
+    nova delete "$CINDER_VM_NAME"
+    echo "Deleting cinder floating ip"
+    nova floating-ip-delete "$CINDER_FLOATING_IP"
 
-set -e
+    set -e
+fi
