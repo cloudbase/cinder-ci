4168650812872b6b6af0943b50efdfddb8a4f324
Costin Galan <cgalan@cloudbasesolutions.com>
2015-11-17 18:36:18 +0200

    Updated retry values

--- jobs/utils.sh
+++ jobs/utils.sh
@@ -35,14 +35,14 @@ run_wsmancmd_with_retry () {
     PASSWORD=$3
     CMD=${@:4}
 
-    exec_with_retry 10 5 "python /var/lib/jenkins/jenkins-master/wsman.py -U https://$HOST:5986/wsman -u $USERNAME -p $PASSWORD $CMD"
+    exec_with_retry 18 10 "python /var/lib/jenkins/jenkins-master/wsman.py -U https://$HOST:5986/wsman -u $USERNAME -p $PASSWORD $CMD"
 }
 
 wait_for_listening_port () {
     HOST=$1
     PORT=$2
     TIMEOUT=$3
-    exec_with_retry 50 5 "nc -z -w$TIMEOUT $HOST $PORT"
+    exec_with_retry 30 10 "nc -z -w$TIMEOUT $HOST $PORT"
 }
 
 run_ssh_cmd () {
@@ -57,7 +57,7 @@ run_ssh_cmd_with_retry () {
     SSHKEY=$2
     CMD=$3
     INTERVAL=$4
-    MAX_RETRIES=10
+    MAX_RETRIES=20
 
     COUNTER=0
     while [ $COUNTER -lt $MAX_RETRIES ]; do
