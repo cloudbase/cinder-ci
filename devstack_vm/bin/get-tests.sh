2422a90de9e3662df47dc459ef9067484e772822
herciunichita <herciu.nichita@gmail.com>
2015-11-18 13:09:58 +0200

    changed run tempest workflow

---
+++ devstack_vm/bin/get-tests.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+array_to_regex()
+{
+    local ar=(${@})
+    local regex=""
+
+    for s in "${ar[@]}"
+    do
+        if [ "$regex" ]; then
+            regex+="\\|"
+        fi
+        regex+="^"$(echo $s | sed -e 's/[]\/$*.^|[]/\\&/g')
+    done
+    echo $regex
+}
+
+project=$1
+tests_dir=$2
+test_suite=${3:-"default"}
+job_type=$4
+
+#determine which tests to exclude/isolate based on the job type
+if [ $job_type = "iscsi" ]; then
+	exclude_tests_file="/home/ubuntu/bin/excluded-tests-"$job_type".txt"
+	isolated_tests_file="/home/ubuntu/bin/isolated-tests-"$job_type".txt"
+else
+	exclude_tests_file="/home/ubuntu/bin/excluded-tests.txt"
+	isolated_tests_file="/home/ubuntu/bin/isolated-tests.txt"
+fi
+
+
+if [ -f "$exclude_tests_file" ]; then
+    exclude_tests=(`awk 'NF && $1!~/^#/' $exclude_tests_file`)
+fi
+
+if [ -f "$isolated_tests_file" ]; then
+    isolated_tests=(`awk 'NF && $1!~/^#/' $isolated_tests_file`)
+fi
+
+exclude_tests=( ${exclude_tests[@]} ${isolated_tests[@]} )
+exclude_regex=$(array_to_regex ${exclude_tests[@]})
+
+cd $tests_dir
+
+if [ ! "$exclude_regex" ]; then
+    exclude_regex='^$'
+fi
+
+
+#if [[ $project == "nova" ]]; then
+    testr list-tests | grep -v $exclude_regex
+#elif [[ $project == "neutron" ]]; then
+
+#    testr list-tests | grep "tempest.api.network" | grep -v $exclude_regex
+#else
+#    echo "ERROR: Cannot test for project $project"
+#    exit 1
+#fi
---
+++ devstack_vm/bin/run-all-tests.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+#cinder-specific job type parameter 
+job_type=$1
+
+project=${1:-"openstack/cinder"}
+tests_dir=${2:-"/opt/stack/tempest"}
+parallel_tests=${3:-4}
+max_attempts=${4:-3}
+test_suite=${5:-"default"}
+log_file=${6:-"/home/ubuntu/tempest/subunit-output.log"}
+results_html_file=${7:-"/home/ubuntu/tempest/results.html"}
+tempest_output_file="/home/ubuntu/tempest/tempest-output.log"
+subunit_stats_file="/home/ubuntu/tempest/subunit_stats.log"
+TEMPEST_DIR="/home/ubuntu/tempest"
+
+basedir="/home/ubuntu/bin"
+
+project_name=$(basename $project)
+
+mkdir -p $TEMPEST_DIR
+
+pushd $basedir
+
+. $basedir/utils.sh
+
+tests_file=$(tempfile)
+$basedir/get-tests.sh $project_name $tests_dir $test_suite $job_type  > $tests_file
+
+echo "Started running tests."
+
+if [ ! -d "$tests_dir/.testrepository" ]; then
+    push_dir
+    cd $tests_dir
+    echo "Initializing testr"
+    testr init
+    pop_dir
+fi
+
+$basedir/parallel-test-runner.sh $tests_file $tests_dir $log_file \
+    $parallel_tests $max_attempts || true
+
+if [[ $job_type == "iscsi" ]]; then
+    isolated_tests_file=$basedir/isolated-tests-iscsi.txt
+    if [ -f "$isolated_tests_file" ]; then
+        echo "Running isolated tests from: $isolated_tests_file"
+        log_tmp=$(tempfile)
+        $basedir/parallel-test-runner.sh $isolated_tests_file $tests_dir $log_tmp \
+            $parallel_tests $max_attempts 1 || true
+
+        cat $log_tmp >> $log_file
+        rm $log_tmp
+    fi
+else
+	isolated_tests_file=$basedir/isolated-tests.txt
+	if [ -f "$isolated_tests_file" ]; then
+		echo "running isolated tests from: $isolated_tests_file"
+		log_tmp=$(tempfile)
+		$basedir/parallel-test-runnner.sh $isolated_tests_file $tests_dir $log_tmp \
+			$parallel_tests $max_attempts 1 || true
+		cat $log_tmp >> $log_file
+		rm $log_tmp
+	fi
+fi
+
+
+
+
+rm $tests_file
+
+echo "Generating HTML report..."
+$basedir/get-results-html.sh $log_file $results_html_file
+
+cat $log_file | /opt/stack/tempest/tools/colorizer.py > $tempest_output_file 2>&1 || true
+
+subunit-stats $log_file > $subunit_stats_file
+exit_code=$?
+
+echo "Total execution time: $SECONDS seconds."
+
+popd
+
+exit $exit_code
