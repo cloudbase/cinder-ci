6de37d49f797a0b4a1581c9008549370410d0f05
Victor Laza <vlaza@cloudbasesolutions.com>
2015-12-11 08:54:00 +0200

    Revert threaded jobs

--- jobs/smb3_linux/initialize.sh
+++ jobs/smb3_linux/initialize.sh
@@ -4,12 +4,5 @@ source /usr/local/src/cinder-ci/jobs/utils.sh
 set -e
 source $KEYSTONERC
 
-# Building devstack as a threaded job
-echo `date -u +%H:%M:%S` "Started to build devstack as a threaded job"
-nohup /usr/local/src/cinder-ci/jobs/deploy_devstack_vm.sh > /home/jenkins-slave/logs/devstack-build-log-$JOB_TYPE-$ZUUL_UUID 2>&1 &
-pid_devstack=$!
-
-# Waiting for devstack threaded job to finish
-wait $pid_devstack
-cat /home/jenkins-slave/logs/devstack-build-log-$JOB_TYPE-$ZUUL_UUID
-
+# Deploy devstack vm
+source /usr/local/src/cinder-ci/jobs/deploy_devstack_vm.sh
