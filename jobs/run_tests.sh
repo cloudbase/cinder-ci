172316c01e271b2bf795b237d77694065e276957
Remus Buzatu <r.buzatu90@gmail.com>
2016-10-06 14:13:52 +0300

    Added support for init only on debug jobs
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- jobs/run_tests.sh
+++ jobs/run_tests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 export FAILURE=0
-echo "Running tests"
-ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $DEVSTACK_SSH_KEY ubuntu@$DEVSTACK_FLOATING_IP "source /home/ubuntu/keystonerc && /home/ubuntu/bin/run-all-tests.sh $JOB_TYPE" 
+if [[ ! -z $RUN_TESTS ]] && [[ $RUN_TESTS == "no" ]]; then
+    echo "Init phase done, not running tests"
+    result_tempest=0
+else
+    echo "Running tests"
+    ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $DEVSTACK_SSH_KEY ubuntu@$DEVSTACK_FLOATING_IP "source /home/ubuntu/keystonerc && /home/ubuntu/bin/run-all-tests.sh $JOB_TYPE"
+    result_tempest=0
+fi
