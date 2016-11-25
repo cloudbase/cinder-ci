2e039b4fd4fd5bb369735b57cb118209aac544f1
Nichita Herciu <nherciu@cloudbasesolutions.com>
2016-11-25 14:57:50 +0200

    fix tempest results bug

--- jobs/run_tests.sh
+++ jobs/run_tests.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-export FAILURE=0
+
 if [[ ! -z $RUN_TESTS ]] && [[ $RUN_TESTS == "no" ]]; then
     echo "Init phase done, not running tests"
     result_tempest=0
 else
     echo "Running tests"
     ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $DEVSTACK_SSH_KEY ubuntu@$DEVSTACK_FLOATING_IP "source /home/ubuntu/keystonerc && /home/ubuntu/bin/run-all-tests.sh $JOB_TYPE"
-    result_tempest=0
+    result_tempest=$?
+    if [ $result_tempest != 0 ]
+    then
+        exit 1
+        echo "Tempest tests failed"
+    fi
 fi
