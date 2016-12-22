b5cb45b258bf2764d80f15673515e867e6672201
Nichita Herciu <nherciu@cloudbasesolutions.com>
2016-12-22 14:16:31 +0200

    update scripts to match new jenkins jobs workflow

---
+++ jobs/run_job.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+jen_date=$(date +%d/%m/%Y-%H:%M)
+export IS_DEBUG_JOB
+set +e
+/usr/local/src/cinder-ci/jobs/$JOB_TYPE/initialize.sh 2>&1
+result_init=$?
+echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;$JOB_TYPE;init;$result_init" >> /home/jenkins-slave/cinder-statistics.log
+echo "Init job finished with exit code $result_init"
+
+if [ $result_init -eq 0 ]; then
+    jen_date=$(date +%d/%m/%Y-%H:%M)
+    if [[ ! -z $RUN_TESTS ]] && [[ $RUN_TESTS == "no" ]]; then
+        echo "Init phase done, not running tests"
+        result_tempest=0
+    else
+        /usr/local/src/cinder-ci/jobs/run_tests.sh 2>&1
+        result_tempest=$?
+        echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;$JOB_TYPE;init;$result_tempest" >> /home/jenkins-slave/cinder-statistics.log
+        echo "Tempest job finished with exit code $result_tempest"
+    fi
+fi
+
+if [ $result_init -eq 0 ] && [ $result_tempest -eq 0 ]; then
+    result=0
+else
+    result=1
+fi
+
+set -e
+exit $result
