89ccda99a5cebd4a00e25202571fd0fe2257a527
Remus Buzatu <r.buzatu90@gmail.com>
2017-01-14 16:21:01 +0200

    Remove devstack VM first then teardown
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- jobs/cleanup.sh
+++ jobs/cleanup.sh
@@ -8,13 +8,13 @@ echo "Collecting logs"
 
 if [ -z "$DEBUG_JOB" ] || [ "$DEBUG_JOB" != "yes" ]; then
     echo "Not a debug job, cleaning up environment."
-    run_wsmancmd_with_retry 3 $hyperv01 $WIN_USER $WIN_PASS 'powershell -ExecutionPolicy RemoteSigned C:\OpenStack\cinder-ci\HyperV\scripts\teardown.ps1'
-    run_wsmancmd_with_retry 3 $hyperv02 $WIN_USER $WIN_PASS 'powershell -ExecutionPolicy RemoteSigned C:\OpenStack\cinder-ci\HyperV\scripts\teardown.ps1'
-    run_wsmancmd_with_retry 3 $ws2012r2 $WIN_USER $WIN_PASS 'powershell -ExecutionPolicy RemoteSigned C:\cinder-ci\windows\scripts\teardown.ps1'
-    
     source /home/jenkins-slave/tools/keystonerc_admin
     nova delete $VMID
     /usr/local/src/cinder-ci/vlan_allocation.py -r $VMID
+    
+    run_wsmancmd_with_retry 3 $hyperv01 $WIN_USER $WIN_PASS 'powershell -ExecutionPolicy RemoteSigned C:\OpenStack\cinder-ci\HyperV\scripts\teardown.ps1'
+    run_wsmancmd_with_retry 3 $hyperv02 $WIN_USER $WIN_PASS 'powershell -ExecutionPolicy RemoteSigned C:\OpenStack\cinder-ci\HyperV\scripts\teardown.ps1'
+    run_wsmancmd_with_retry 3 $ws2012r2 $WIN_USER $WIN_PASS 'powershell -ExecutionPolicy RemoteSigned C:\cinder-ci\windows\scripts\teardown.ps1'
 else
     echo "Not cleaning up because debug job variable is set to true."
 fi
