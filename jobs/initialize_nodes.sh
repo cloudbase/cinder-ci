2190556b121dfaf4cd46f5c9dca9b0f3f9cce09c
Andrei Bacos <andreibacos@gmail.com>
2017-04-13 01:39:42 +0300

    change apt proxy

--- jobs/initialize_nodes.sh
+++ jobs/initialize_nodes.sh
@@ -151,7 +151,7 @@ scp -v -r -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i $DE
 
 # Repository section
 echo "setup apt-cacher-ng:"
-run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP $DEVSTACK_SSH_KEY 'echo "Acquire::http { Proxy \"http://10.20.1.32\" };" | sudo tee --append /etc/apt/apt.conf.d/90-apt-proxy.conf' 12
+run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP $DEVSTACK_SSH_KEY 'echo "Acquire::http { Proxy \"http://10.20.1.36:8000\" };" | sudo tee --append /etc/apt/apt.conf.d/90-apt-proxy.conf' 12
 echo "clean any apt files:"
 run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP $DEVSTACK_SSH_KEY "sudo rm -rfv /var/lib/apt/lists/*" 12
 echo "apt-get update:"
